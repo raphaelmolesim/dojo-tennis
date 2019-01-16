811644dbd997d8a9720bf1cecd56d32683e9f6fa
Raphael Molesim <raphael.molesim@gmail.com>
2019-01-16 19:20:01 -0200

    Trabalho em proogresso

---
+++ spec/tennis_spec.rb
@@ -0,0 +1,25 @@
+require "rspec"
+require "./tennis"
+
+describe "Tennis" do
+  
+  it "deveria marcar 15x0 quando jogador A faz um ponto" do
+    placar = Tennis.new
+    placar.computa_jogada("JogadorA")
+    expect(placar.qtd_pontos_jogador_a).to be(15)
+    expect(placar.qtd_pontos_jogador_b).to be(0)
+  end
+  
+  it "deveria marcar 15x15 quando jogador A faz um ponto e o jogadorB tambem" do
+    placar = Tennis.new
+    placar.computa_jogada("JogadorA")
+
+    placar.computa_jogada("JogadorB")
+    expect(placar.qtd_pontos_jogador_a).to be(15)
+    expect(placar.qtd_pontos_jogador_b).to be(15)
+  end
+  
+  
+  
+  
+end
---
+++ tennis.rb
@@ -0,0 +1,20 @@
+class Tennis
+  attr_accessor :qtd_pontos_jogador_a
+  attr_accessor :qtd_pontos_jogador_b
+  
+  def initialize
+    @qtd_pontos_jogador_a = 0
+    @qtd_pontos_jogador_b = 0
+  end
+  
+  
+  def computa_jogada jogador
+    if(jogador == "JogadorA")
+      @qtd_pontos_jogador_a = 15
+    end
+    if(jogador == "JogadorB")
+      @qtd_pontos_jogador_b = 15
+    end
+  end
+  
+end
